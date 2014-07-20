d86150710c8867dfd76ea200182137257c670e2c
Max Kuipers <max.kuipers@centro.net>
2014-07-20 14:07:55 -0500

    0 configuration, end to end talking. spike complete

--- README.md
+++ README.md
@@ -3,8 +3,16 @@ sublime_lorem_ipsum
 
 Server and client for providing lorem ipsum to a Sublime Text plugin
 
-
+## Installation
 ```
 mkdir ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/LoremIpsumConsumer
 ln -s #{repository_root}/lorem_ipsum_consumer/LoremIpsumConsumer.py ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/LoremIpsumConsumer/LoremIpsumConsumer.py
 ```
+
+Get [pow](http://pow.cx).
+
+`cd lorem_ipsum_provider`
+
+`bundle install`
+
+`powder link`
--- lorem_ipsum_consumer/LoremIpsumConsumer.py
+++ lorem_ipsum_consumer/LoremIpsumConsumer.py
@@ -1,5 +1,18 @@
 import sublime, sublime_plugin
+import urllib
+import urllib2
+import threading
 
-class ExampleCommand(sublime_plugin.TextCommand):
+class LoremIpsumCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    self.view.insert(edit, 0, "Hello, World!")
+    try:
+      request = urllib2.Request('http://lorem-ipsum-provider.dev/filler.json')
+      http_file = urllib2.urlopen(request, timeout=10)
+      self.view.insert(edit, 0, http_file.read())
+      return
+    except (urllib2.HTTPError) as (e):
+        err = '%s: HTTP error %s contacting API' % (__name__, str(e.code))
+    except (urllib2.URLError) as (e):
+        err = '%s: URL error %s contacting API' % (__name__, str(e.reason))
+    sublime.error_message(err)
+    self.result = False

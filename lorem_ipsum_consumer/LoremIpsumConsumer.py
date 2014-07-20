44095ad89416b1bc485cf30ed0d7e6c2b8c7725d
Max Kuipers <max.kuipers@centro.net>
2014-07-20 17:33:56 -0500

    *hangs head shamefully* this python needs some serious refactoring. hopefully i can find someone who knows python better

--- lorem_ipsum_consumer/LoremIpsumConsumer.py
+++ lorem_ipsum_consumer/LoremIpsumConsumer.py
@@ -8,16 +8,23 @@ class PromptLoremIpsumCommand(sublime_plugin.WindowCommand):
   def run(self):
     self.window.show_input_panel("Number of Paragraphs:", "1", self.on_done, None, None)
 
-  def on_done(self, paragraphs):
-    # TODO: validate that the input is a number
-    self.window.active_view().run_command('lorem_ipsum', { "paragraphs": paragraphs} )
+  def on_done(self, input_value):
+    # TODO: cry yourself to sleep tonight because you're so ashamed of this
+    try:
+      self.window.active_view().run_command('lorem_ipsum', { "paragraphs": int(input_value), "template": None } )
+    except (ValueError):
+      self.window.active_view().run_command('lorem_ipsum', { "paragraphs": None, "template": input_value } )
+
 
 class LoremIpsumCommand(sublime_plugin.TextCommand):
-  def run(self, edit, paragraphs):
+  def run(self, edit, paragraphs, template):
     sels = self.view.sel()
     for sel in sels:
       try:
-        data = urllib.urlencode({'paragraphs': paragraphs})
+        if template is None:
+          data = urllib.urlencode({'paragraphs': paragraphs})
+        else:
+          data = urllib.urlencode({'template': template})
         request = urllib2.Request('http://lorem-ipsum-provider.dev/filler.json?' + data)
         http_file = urllib2.urlopen(request, timeout=10)
         self.view.replace(edit, sel, http_file.read())

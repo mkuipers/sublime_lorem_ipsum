e1512c438f9b409784d6965f014209ab7182d090
Max Kuipers <max.kuipers@centro.net>
2014-07-20 13:00:43 -0500

    inital commit

---
+++ lorem_ipsum_provider/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LoremIpsumProvider::Application.config.secret_key_base = 'fd7e4bb29ab88681dadc9d6684d57a9c238b8c6e334456cd18363054f7fff96d9e95854fcd194516f1dd2696c9085651cbde7bfc8a9ece590fe52d4e974985a6'
---
+++ lorem_ipsum_provider/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoremIpsumProvider::Application.config.session_store :cookie_store, key: '_lorem_ipsum_provider_session'

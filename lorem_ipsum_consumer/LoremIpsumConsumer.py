18780e7ab492a1a31ef6e64b3a46739625d8adbf
Max Kuipers <max.kuipers@centro.net>
2014-07-20 15:31:24 -0500

    opens a prompt, asking for the number of paragraphs to insert now

--- lorem_ipsum_consumer/LoremIpsumConsumer.py
+++ lorem_ipsum_consumer/LoremIpsumConsumer.py
@@ -2,12 +2,22 @@ import sublime, sublime_plugin
 import urllib
 import urllib2
 
+
+
+class PromptLoremIpsumCommand(sublime_plugin.WindowCommand):
+  def run(self):
+    self.window.show_input_panel("Number of Paragraphs:", "1", self.on_done, None, None)
+
+  def on_done(self, paragraphs):
+    # TODO: validate that the input is a number
+    self.window.active_view().run_command('lorem_ipsum', { "paragraphs": paragraphs} )
+
 class LoremIpsumCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
+  def run(self, edit, paragraphs):
     sels = self.view.sel()
     for sel in sels:
       try:
-        data = urllib.urlencode({'paragraphs': 5})
+        data = urllib.urlencode({'paragraphs': paragraphs})
         request = urllib2.Request('http://lorem-ipsum-provider.dev/filler.json?' + data)
         http_file = urllib2.urlopen(request, timeout=10)
         self.view.replace(edit, sel, http_file.read())
